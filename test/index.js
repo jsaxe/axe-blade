ba035787012dbbaee8eb7b2bcbfc7e086ecbbe26
Robin Panta <hacktivistic@gmail.com>
2019-02-10 23:39:48 +0530

    [test] load test cases

---
+++ test/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Events
+require('./Events/EmitterTest')
+
+// Log
+require('./Log/LoggerTest')
+
+// Config
+require('./Config/ConfigTest')
+
+// Env
+require('./Env/EnvTest')
+
+// Application
+require('./Application/ApplicationTest')
+
+// Application Integration
+require('./Integration/ApplicationTest')
+
