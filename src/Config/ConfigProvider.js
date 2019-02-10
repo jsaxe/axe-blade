25f705a72a6aab4f289cf790a8f4dd54a9c3abc0
Robin Panta <hacktivistic@gmail.com>
2019-02-10 23:34:04 +0530

    [feat] create config service

---
+++ src/Config/ConfigProvider.js
@@ -0,0 +1,23 @@
+/**
+ * @name ConfigProvider
+ * @author Jeevan Prakash Pant <jeevanppant@gmail.com>
+ */
+
+'use strict'
+
+var ServiceProvider = require('../Application/ServiceProvider')
+var Config = require('./Config')
+
+class ConfigProvider extends ServiceProvider{
+
+	register () {
+
+		this.app.singleton('Axe/Config', function (app) {
+			return new Config(app.use('axe.path.config'))
+		})
+
+	}
+
+}
+
+module.exports = ConfigProvider
