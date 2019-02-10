3961701e605ddf591bf7b1713a076eb21ebcf96b
Robin Panta <hacktivistic@gmail.com>
2019-02-10 23:34:21 +0530

    [test] test config service

---
+++ test/Config/ConfigTest.js
@@ -0,0 +1,38 @@
+'use strict'
+
+var expect = require('chai').expect;
+
+var Config = require('../../lib/Config/Config')
+var Application = require('../../lib/Application/Application')
+
+describe('Config', function () {
+
+	var app = new Application('test/Config/TestBasePath')
+	var config = new Config(app.use('axe.path.config'))
+
+	it('should preload console files from config dir', function () {
+
+		var expectedConfigPath = require('path').resolve('./test/Config/TestBasePath/config')
+		expect(config._configPath).to.equal(expectedConfigPath)
+
+	})
+
+	describe('get()', function () {
+
+		it('should fetch the config\'s setting', function () {
+
+			var expectedValue = {
+				foo : 'bar'
+			}
+			expect(config.get('test')).to.deep.equal(expectedValue)
+			expect(config.get('test.foo')).to.equal('bar')
+
+		})
+
+		it('should return default value when config is not found', function () {
+			expect(config.get('test.baz', 'tutti')).to.equal('tutti')
+		})
+
+	})
+
+})
