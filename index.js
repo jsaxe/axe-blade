be9c5ccbc6fc1de8e6767eae5f84b50de4a6043c
Robin Panta <hacktivistic@gmail.com>
2019-02-10 23:38:06 +0530

    [feat] export application and service provider

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * axe-engine
+ * Copyright(c) 2018
+ * MIT Licensed
+ *
+ * @author Robin Panta <hacktivistic@gmail.com>
+ */
+
+'use strict'
+
+/**
+ * Bootstraping using the Demon Slayer Axe to get our application
+ */
+var Application = require('./lib/Application/Application')
+var ServiceProvider = require('./lib/Application/ServiceProvider')
+
+/**
+ * Finally trading blows (slashing with Demon Slayer Axe)
+ */
+module.exports = { Application, ServiceProvider }
+
+
+// Encryption, Exceptions, FileSystem, Hash, Helpers
