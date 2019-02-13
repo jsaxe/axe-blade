c08269d6a1d81fa36f5a5c5fad403e25a8cbce21
Robin Panta <hacktivistic@gmail.com>
2019-02-13 15:03:23 +0530

    [refactor] change config path

--- src/Config/ConfigProvider.js
+++ src/Config/ConfigProvider.js
@@ -13,7 +13,7 @@ class ConfigProvider extends ServiceProvider{
 	register () {
 
 		this.app.singleton('Axe/Config', function (app) {
-			return new Config(app.use('axe.path.config'))
+			return new Config(app.configPath())
 		})
 
 	}
